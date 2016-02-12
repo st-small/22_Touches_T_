15eb273a27a6f4124226eb7339f25acce98519f9
Stanly Shiyanovskiy <st-small@rambler.ru>
2016-02-12 10:15:33 +0200

    Initial Commit

---
+++ 22_Touches_T_/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  22_Touches_T_
+//
+//  Created by Stas on 12.02.16.
+//  Copyright © 2016 Stas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 22_Touches_T_/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  22_Touches_T_
+//
+//  Created by Stas on 12.02.16.
+//  Copyright © 2016 Stas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
